822635c506362ee2246b1477dec0aae0a2953410
blind <ben.lind@grantstreet.com>
2017-05-23 10:03:47 -0400

    Add smart quote replacement function

--- functions.el
+++ functions.el
@@ -180,6 +180,15 @@
   (goto-char (point-min))
   (forward-paragraph arg))
 
+(defun replace-smart-quotes (beg end)
+  "Replace 'smart quotes' in buffer or region with ascii quotes."
+  (interactive "r")
+  (format-replace-strings '(("\x201C" . "\"")
+                            ("\x201D" . "\"")
+                            ("\x2018" . "'")
+                            ("\x2019" . "'"))
+                          nil beg end))
+
 
 ;;; CUSTOMIZE NEWLINE FUNCTION
 
