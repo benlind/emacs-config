282040737d461e5dda416955683121ced5ce945f
Ben Lind <bendlind@gmail.com>
2018-04-26 09:07:07 -0400

    Disable projectile caching
    The caching is annoying when you're constantly adding new files to a repo. It's
    not slow in reasonably-sized repos.

--- modes.el
+++ modes.el
@@ -246,7 +246,7 @@
 ;;
 ;; Running `C-u C-c p f' will invalidate the cache prior to prompting you for a
 ;; file to jump to.
-(setq projectile-enable-caching t)
+;; (setq projectile-enable-caching t)
 
 
 ;;; HELM
