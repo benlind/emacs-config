0a4bac859f2aff6f63f86ec8bd8798a8cb5cfdf9
Ben Lind <bendlind@gmail.com>
2018-11-30 13:19:14 -0500

    Indent inline scripts and styles with 2 spaces

--- modes.el
+++ modes.el
@@ -196,6 +196,9 @@
   (setq web-mode-code-indent-offset               2)
   (setq web-mode-css-indent-offset                2)
   (setq web-mode-markup-indent-offset             2)
+  (setq web-mode-script-padding                   2) ; indenting inline scripts (default 1)
+  (setq web-mode-style-padding                    2) ; inline styles
+  (setq web-mode-block-padding                    2) ; inline server code like PHP
   (setq web-mode-enable-css-colorization          t)
   (setq web-mode-enable-current-element-highlight t)
   )
